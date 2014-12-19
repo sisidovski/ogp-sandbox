99f5302f70affaf44707c16641c056c9eca2cf79
ssd <shunya_shishido@dwango.co.jp>
2014-12-19 15:23:55 +0900

    add ogp inspect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/twitter', {
+        templateUrl: 'views/twitter.html',
+        controller: 'TwitterCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/twitter.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ogpSandboxApp.controller:TwitterCtrl
+ * @description
+ * # TwitterCtrl
+ * Controller of the ogpSandboxApp
+ */
+angular.module('ogpSandboxApp')
+  .controller('TwitterCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
